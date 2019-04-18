ab6b80f44427384b7b8cd8abea0669687bc8b608
Elizabeth Boahen <elizboahen@gmail.com>
2019-04-17 20:55:13 -0400

    Singly linked list done

--- src/main/java/edu/smith/cs/csc212/adtr/real/SinglyLinkedList.java
+++ src/main/java/edu/smith/cs/csc212/adtr/real/SinglyLinkedList.java
@@ -138,11 +138,17 @@ public class SinglyLinkedList<T> extends ListADT<T> {
 		throw new BadIndexError(index);
 	}
 	
-
 	@Override
 	public void setIndex(int index, T value) {
 		checkNotEmpty();
-		throw new TODOErr();
+		int at = 0;
+		for (Node<T> n = this.start; n != null; n = n.next) {
+			if (at++ == index) {
+				n.value = value;
+				return;
+			}
+		}
+		throw new BadIndexError(index);
 	}
 
 	@Override
